f3fa12c4b3ed8d9897918bef1971246c7bd8bec0
Anthony19H <anthony.fabian.herrera@hotmail.com>
2026-04-11 22:35:21 -0500

    Parte 5 cuenta regresiva

--- cazando.js
+++ cazando.js
@@ -18,7 +18,8 @@ let comidaY= 0;
 //velocidad
 const VELOCIDAD = 15;
 
-let puntaje=0
+let puntaje=0;
+let tiempo=10;
 
 function graficarRectangulo(ejeX,ejeY,ancho,altura,color){
     ctx.fillStyle = color;
@@ -35,6 +36,7 @@ function graficarComida() {
 };
 
 function iniciarJuego(){
+    setInterval(restarTiempo,1000);
     gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);     
     gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
     comidaX=generarAleatorio(0,canvas.width-ANCHO_COMIDA);
@@ -92,7 +94,7 @@ function detectarColision(){
     if(gatoX < comidaX+ANCHO_COMIDA && gatoX + ANCHO_GATO > comidaX &&
     gatoY < comidaY + ALTO_COMIDA && gatoY + ALTO_GATO > comidaY){
         
-        alert("Atrapada")
+        alert("Atrapadaaaaaaa!!")
         moverComida();
         puntaje=puntaje+1;
         mostrarEnSpan("puntos",puntaje)
@@ -106,6 +108,10 @@ function detectarColision(){
 function moverComida(){
     comidaX=generarAleatorio(0,canvas.width-ANCHO_COMIDA);
     comidaY=generarAleatorio(0,canvas.width-ALTO_COMIDA);
+}
+function restarTiempo(){
+    tiempo=tiempo-1
+    mostrarEnSpan("tiempo",tiempo);
 }
 document.getElementById("btnArriba").onclick = () => moverArriba();
 document.getElementById("btnAbajo").onclick = () => moverAbajo();
